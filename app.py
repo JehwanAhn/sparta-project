119d33a9a97f72ad856a57c0bd80488b636beb06
JehwanAhn <dkswpghks@naver.com>
2021-10-07 19:05:42 +0900

    11th commit

--- app.py
+++ app.py
@@ -83,8 +83,11 @@ def youtube_get():
     for i in cm['items']:
         videos.append(i)
 
+
+
     print(videos)
     return jsonify({'all_videos': videos})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
